e644eea206745295be2e06d0795cc1ffa71ee7a5
Nguyen Ut <uttester.tb@gmail.com>
2020-11-30 23:07:19 +0700

    Web Application

--- selenium/webdriver/Test_Zingme.java
+++ selenium/webdriver/Test_Zingme.java
@@ -3,8 +3,11 @@ package webdriver;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,7 +17,9 @@ import org.testng.annotations.Test;
 public class Test_Zingme {
 	WebDriver driver;
 	Webelement element;
-
+	Select select;
+	
+	
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
@@ -25,24 +30,43 @@ public class Test_Zingme {
 
 	@Test
 	public void TC_01() {
-		driver.get("https://egov.danang.gov.vn/reg");
-		
+		String loginUrl = driver.getCurrentUrl();
+		driver.get("https://id.zing.vn/");
+		driver.findElement(By.xpath("//a[text()='Đăng ký ngay']")).click();
+		driver.findElement(By.id("regacc_fullname")).sendKeys("Nguyen Thi Ut");
+		driver.findElement(By.id("regacc_account")).sendKeys("NguyenUt");
+		driver.findElement(By.id("regacc_pwd")).sendKeys("Ut26031991");
+		driver.findElement(By.id("regacc_re_pwd")).sendKeys("Ut26031991");
+		select = new Select(driver.findElement(By.id("dob")));
+		select.selectByVisibleText("26");
+		select = new Select(driver.findElement(By.id("mob")));
+		select.selectByVisibleText("03");
+		select = new Select(driver.findElement(By.id("yob")));
+		select.selectByVisibleText("1991");
+		driver.findElement(By.xpath("//input[@value='0']")).click();
+		driver.findElement(By.xpath("//a[text()='Đăng ký']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//h3[text()='Bạn đã đăng ký Zing ID thành công']")).isDisplayed());
+		System.out.println("Tạo Tài Khoản thành công");
+		driver.get(loginUrl);
+		driver.findElement(By.id("login_account")).sendKeys("NguyenUt");
+		driver.findElement(By.id("login_pwd")).sendKeys("Ut26031991");
+		driver.findElement(By.xpath("//a[text()='Đăng nhập']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Zing ID']")).isDisplayed());
+		System.out.println("Đăng nhập thành công");
 		
 	}
-	@Test
+
 	public void TC_02() {
 		driver.get("https://egov.danang.gov.vn/reg");
 		
 		
 	}
-	@Test
+	
 	public void TC_03() {
 		driver.get("https://egov.danang.gov.vn/reg");
 		
 		
 	}
-	
-	
 
 	
 	@AfterClass
